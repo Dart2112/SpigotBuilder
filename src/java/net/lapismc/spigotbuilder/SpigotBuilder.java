3917e309d50dba4a52ae01d2f0d57012a02857fc
dart2112 <DNAblue2112@gmail.com>
2019-01-15 18:23:44 +1030

    IO Update Moved to Apache Commons IO for file copying Added jar integrity check when copying the file to be installed

--- src/java/net/lapismc/spigotbuilder/SpigotBuilder.java
+++ src/java/net/lapismc/spigotbuilder/SpigotBuilder.java
@@ -2,7 +2,7 @@ package net.lapismc.spigotbuilder;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
-import org.bukkit.util.FileUtil;
+import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -32,10 +32,11 @@ class SpigotBuilder {
         //Check if we have the latest version
         File currentSpigotJar = new File("./Spigot.jar");
         log("Checking for updates to Spigot");
-        if (mustUpdate(currentSpigotJar)) {
+        int mustUpdate = mustUpdate(currentSpigotJar);
+        if (mustUpdate != 0) {
             log("Compiling the latest version");
             //Run build tools if we dont have the latest version
-            runBuildTools(buildDir, currentSpigotJar);
+            runBuildTools(buildDir, currentSpigotJar, mustUpdate == 2);
         }
         //Set EULA file before starting the server
         placeEula();
@@ -44,20 +45,29 @@ class SpigotBuilder {
         System.exit(0);
     }
 
-    private boolean mustUpdate(File currentSpigotJar) {
-        boolean mustUpdate = false;
+    /**
+     * Check for an update to Spigot
+     *
+     * @param currentSpigotJar The current jar file for spigot
+     * @return Returns 0 if there is no update, 1 if there isn't a valid jar, 2 if the update could be downloaded in the background
+     */
+    private int mustUpdate(File currentSpigotJar) {
+        File updatedJar = new File("./Spigot-UPDATED");
+        if (updatedJar.exists()) {
+            updatedJar.renameTo(currentSpigotJar);
+            log("Update installed!");
+            return 0;
+        }
+        int mustUpdate = 0;
         if (currentSpigotJar.exists()) {
             try {
-                InputStream stream = new FileInputStream(currentSpigotJar);
-                JarInputStream jarStream = new JarInputStream(stream);
-                Manifest mf = jarStream.getManifest();
-                String version = mf.getMainAttributes().getValue("Implementation-Version");
+                String version = getVersion(currentSpigotJar);
                 String[] parts = version.substring("git-Spigot-".length()).split("-");
                 int spigotVersions = getDistance("spigot", parts[0]);
                 int craftBukkitVersions = getDistance("craftbukkit", parts[1]);
                 //If the difference is 0 we dont update
-                mustUpdate = !(spigotVersions == 0 && craftBukkitVersions == 0);
-                if (mustUpdate) {
+                mustUpdate = !(spigotVersions == 0 && craftBukkitVersions == 0) ? 2 : 0;
+                if (mustUpdate == 2) {
                     log("You are " + (spigotVersions + craftBukkitVersions) + " version(s) behind the latest Spigot");
                 } else {
                     log("You are on the latest version of Spigot");
@@ -67,12 +77,12 @@ class SpigotBuilder {
             }
         } else {
             log("No Spigot jar found");
-            mustUpdate = true;
+            mustUpdate = 1;
         }
         return mustUpdate;
     }
 
-    private void runBuildTools(File buildDir, File currentSpigotJar) {
+    private void runBuildTools(File buildDir, File currentSpigotJar, boolean background) {
         //Run build tools and copy result into server
         //Check if build tools exists
         File buildJar = new File("./build" + File.separator + "BuildTools.jar");
@@ -97,22 +107,43 @@ class SpigotBuilder {
         ProcessBuilder pb = new ProcessBuilder("java", "-jar", buildJar.getAbsolutePath());
         pb.directory(buildDir);
         try {
-            log("Starting BuildTools, all BuildTools output will not have time stamps");
-            Process process = startProcess(pb, false);
-            process.waitFor();
+            if (background) {
+                log("Starting BuildTools, the update will be installed on the next server start if it is ready");
+            } else {
+                log("Starting BuildTools, all BuildTools output will not have time stamps");
+            }
+            Process process = startProcess(pb, false, !background);
+            if (!background) {
+                process.waitFor();
+            } else {
+                new Thread(() -> {
+                    try {
+                        while (process.isAlive()) {
+                            Thread.sleep(100);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (copyFile(getCompiledJar(buildDir), new File("./Spigot-UPDATED"))) {
+                        log("Update downloaded, it will be installed the next time the server starts");
+                    } else {
+                        log("The jar file could not be installed, this may be because build tools failed or because the destination file is in use");
+                    }
+                }).start();
+            }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             System.out.println("Unable to run build tools! See the above error");
             return;
         }
-        log("Copying the compiled jar");
-        File newSpigotJar = new File("");
-        for (File f : Objects.requireNonNull(buildDir.listFiles())) {
-            if (!f.isDirectory() | f.getName().startsWith("spigot-")) {
-                newSpigotJar = f;
+        if (!background) {
+            log("Copying the compiled jar");
+            if (copyFile(getCompiledJar(buildDir), currentSpigotJar)) {
+                log("Jar file installed");
+            } else {
+                log("The jar file could not be installed, this may be because build tools failed or because the destination file is in use");
             }
         }
-        FileUtil.copy(newSpigotJar, currentSpigotJar);
     }
 
     private void placeEula() {
@@ -138,14 +169,13 @@ class SpigotBuilder {
     private void startServer(File spigotJar) {
         try {
             ProcessBuilder pb = new ProcessBuilder("java", "-jar", spigotJar.getAbsolutePath());
-            startProcess(pb, true);
+            startProcess(pb, true, true);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private Process startProcess(ProcessBuilder pb, boolean allowInput) throws IOException {
-        pb.redirectErrorStream(true);
+    private Process startProcess(ProcessBuilder pb, boolean allowInput, boolean showOutput) throws IOException {
         Process process = pb.start();
         if (allowInput) {
             Thread thread = new Thread(() -> {
@@ -165,11 +195,14 @@ class SpigotBuilder {
             });
             thread.start();
         }
-        InputStream is = process.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+        if (showOutput) {
+            pb.redirectErrorStream(true);
+            InputStream is = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
         }
         return process;
     }
@@ -191,6 +224,41 @@ class SpigotBuilder {
             e.printStackTrace();
             return -1;
         }
+    }
+
+    private File getCompiledJar(File buildDir) {
+        File newSpigotJar = null;
+        for (File f : Objects.requireNonNull(buildDir.listFiles())) {
+            if (!f.isDirectory() | f.getName().startsWith("spigot-")) {
+                newSpigotJar = f;
+            }
+        }
+        return newSpigotJar;
+    }
+
+    private boolean copyFile(File source, File dest) {
+        try {
+            if (source.getName().endsWith(".jar")) {
+                //Verify the files integrity as a jar
+                getVersion(source);
+                String version = getVersion(source);
+                if (!version.startsWith("git-Spigot-")) {
+                    return false;
+                }
+            }
+            FileUtils.copyFile(source, dest);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private String getVersion(File jarFile) throws IOException {
+        InputStream stream = new FileInputStream(jarFile);
+        JarInputStream jarStream = new JarInputStream(stream);
+        Manifest mf = jarStream.getManifest();
+        return mf.getMainAttributes().getValue("Implementation-Version");
     }
 
     private void log(String message) {
